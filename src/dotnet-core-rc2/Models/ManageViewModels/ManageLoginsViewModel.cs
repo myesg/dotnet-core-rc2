4d1c3755faa8d5ea15a88c60773d9856f0090fbb
'selvacodes' <'selvaganapathy@outlook.in'>
2016-06-11 14:22:19 +0530

    Add project files.

---
+++ src/dotnet-core-rc2/Models/ManageViewModels/ManageLoginsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.AspNetCore.Identity;
+
+namespace dotnet_core_rc2.Models.ManageViewModels
+{
+    public class ManageLoginsViewModel
+    {
+        public IList<UserLoginInfo> CurrentLogins { get; set; }
+
+        public IList<AuthenticationDescription> OtherLogins { get; set; }
+    }
+}
